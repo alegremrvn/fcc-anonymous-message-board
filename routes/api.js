209c6d83c7afa0577e16853de981fd3fef7aa9be
Marvin Alegre <alegremrvn@protonmail.com>
2023-03-01 11:35:17 +0800

    good enough

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,186 @@
 'use strict';
 
+const { ObjectId } = require('mongodb')
+
+let threads = {}
+
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+
+  app.route('/api/threads/:board')
+
+    .post((req, res) => {
+      let board = req.params.board
+
+      if (!threads.hasOwnProperty(board)) {
+        threads[board] = []
+      }
+
+      let thread = {
+        _id: new ObjectId(),
+        text: req.body.text,
+        delete_password: req.body.delete_password,
+        replies: [],
+        replycount: 0,
+        created_on: new Date(),
+        bumped_on: new Date(),
+        reported: false
+      }
+
+      threads[board].push(thread)
+      threads[board].sort((a, b) => b.bumped_on - a.bumped_on)
+
+      res.send('go back then refresh.')
+    })
+
+    .get((req, res) => {
+      let board = req.params.board
+
+      if (!threads.hasOwnProperty(board)) {
+        threads[board] = []
+      }
+
+      let output = JSON.parse(JSON.stringify(threads[board]))
+      for (let thread of output) {
+        delete thread.delete_password
+        delete thread.reported
+
+        if (thread.replycount > 3) {
+          thread.replies = thread.replies.slice(thread.replycount - 3, thread.replycount)
+        }
+      }
+
+      res.json(output)
+    })
+
+    .delete((req, res) => {
+      let board = req.params.board
+
+      const threadsCount = threads[board].length
+      let count = 0
+      for (let i = 0; i < threads[board].length; i++) {
+        if (threads[board][i]._id.equals(new ObjectId(req.body.thread_id)) &&
+        threads[board][i].delete_password === req.body.delete_password) {
+          threads[board].splice(i, 1)
+          break
+        }
+        count++
+      }
+
+      if (count === threadsCount) {
+        res.json({
+          not_deleted: 'incorrect password'
+        })
+      } else {
+        res.json({
+          deleted: 'success'
+        })
+      }
+    })
+
+    .put((req, res) => {
+      const board = req.params.board
+
+      res.status(200)
+        .type('text')
+        .send('reported')
+    })
+
+  app.route('/api/replies/:board')
+
+    .post((req, res) => {
+      const board = req.params.board
+      const thread_id = req.body.thread_id
+      const date = new Date()
+
+      let reply = {
+        _id: new ObjectId(),
+        text: req.body.text,
+        delete_password: req.body.delete_password,
+        created_on: date,
+        reported: false
+      }
+
+      let thread
+      for (let obj of threads[board]) {
+        if (obj._id.equals(new ObjectId(thread_id))) {
+          thread = obj
+          break
+        }
+      }
+
+      thread.replies.push(reply)
+      thread.bumped_on = date
+      thread.replycount++
+      threads[board].sort((a, b) => b.bumped_on - a.bumped_on)
+
+      res.send('go back then refresh.')
+    })
+
+    .get((req, res) => {
+      try {
+        const board = req.params.board
+        const thread_id = req.query.thread_id
+
+        let output
+        for (let obj of threads[board]) {
+          if (obj._id.equals(new ObjectId(thread_id))) {
+            output = JSON.parse(JSON.stringify(obj))
+            break
+          }
+        }
+
+        delete output.delete_password
+        delete output.reported
+
+        for (let reply of output.replies) {
+          delete reply.delete_password
+          delete reply.reported
+        }
+
+        res.json(output)
+      } catch (err) {
+        console.log(err)
+      }
+    })
+
+    .delete((req, res) => {
+      const board = req.params.board
+      const thread_id = req.body.thread_id
+
+      const repliesCount = threads[board][0].replies.length
+      let count = 0
+      for (let i = 0; i < threads[board][0].replies.length; i++) {
+        if (threads[board][0].replies[i]._id.equals(new ObjectId(req.body.reply_id)) &&
+        threads[board][0].replies[i].delete_password === req.body.delete_password) {
+          threads[board][0].replies.splice(i, 1)
+          break
+        }
+        count++
+      }
+
+      if (count === repliesCount) {
+        res.json({
+          not_deleted: 'incorrect password'
+        })
+      } else {
+        res.json({
+          deleted: 'success'
+        })
+      }
+      
+    })
+
+    .put((req, res) => {
+      const board = req.params.board
+
+      res.status(200)
+        .type('text')
+        .send('reported')
+    })
+
+  app.route('/api/dbdump')
+    .get((req, res) => {
+      res.json(threads)
+    })
 
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,235 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+const THREADSENDPOINT = '/api/threads/general'
+const REPLIESENDPOINT = '/api/replies/general'
 
+let thread1Id
+let thread2Id
+let reply1Id
+let reply2Id
+
+describe('Functional Tests', function () {
+  describe('Threads', function () {
+    before(function (done) {
+      const text = 'thread1'
+      const delete_password = 'delete thread1'
+
+      chai.request(server)
+        .post(THREADSENDPOINT)
+        .send({
+          text,
+          delete_password
+        })
+        .end(function (err, res) {
+        })
+
+        chai.request(server)
+        .post(THREADSENDPOINT)
+        .send({
+          text: 'thread2',
+          delete_password: 'delete thread2'
+        })
+        .end(function (err, res) {
+          done()
+        })
+    })
+
+    it('#1) creating a new thread', function (done) {
+      chai.request(server)
+        .get(THREADSENDPOINT)
+        .end(function (err, res) {
+          const text = 'thread2'
+          const date = new Date()
+
+          assert.equal(res.status, 200)
+          assert.equal(res.body[0].text, text);
+          assert.isNotNull(res.body[0]._id);
+          assert.equal(new Date(res.body[0].created_on).toDateString(), date.toDateString());
+          assert.equal(res.body[0].bumped_on, res.body[0].created_on);
+          assert.isArray(res.body[0].replies);
+          assert.equal(res.body[0].replycount, 0)
+
+          thread1Id = res.body[1]._id
+          thread2Id = res.body[0]._id
+
+          done()
+        })
+    })
+
+    it('#2) viewing the 10 most recent threads with 3 replies each', function (done) {
+      chai.request(server)
+        .get(THREADSENDPOINT)
+        .end(function (err, res) {
+          assert.isAtMost(res.body.length, 10)
+          for (let i = 0; i < res.body.length; i++) {
+            assert.containsAllKeys(res.body[i], ["_id", "text", "created_on", "bumped_on", "replies"]);
+            assert.isAtMost(res.body[i].replies.length, 3);
+            assert.notExists(res.body[i].delete_password);
+            assert.notExists(res.body[i].reported);
+            assert.isAtMost(res.body[i].replies.length, 3);
+            for (let j = 0; j < res.body[i].replies.length; j++) {
+              assert.notExists(res.body[i].replies[j].delete_password);
+              assert.notExists(res.body[i].replies[j].reported);
+            }
+          }
+
+          done()
+        })
+    })
+
+    it('#3) deleting a thread with the incorrect password', function (done) {
+      chai.request(server)
+        .delete(THREADSENDPOINT)
+        .send({
+          thread_id: thread1Id,
+          delete_password: 'incorrect password'
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.not_deleted, 'incorrect password')
+
+          done()
+        })
+    })
+
+    it('#4) deleting a thread with the correct password', function (done) {
+      chai.request(server)
+        .delete(THREADSENDPOINT)
+        .send({
+          thread_id: thread2Id,
+          delete_password: 'delete thread2'
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.deleted, 'success')
+
+          done()
+        })
+    })
+    
+    it('#5) reporting a thread', function (done) {
+      chai.request(server)
+        .put(THREADSENDPOINT)
+        .send({
+          thread_id: thread1Id
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200)
+          assert.equal(res.text, 'reported')
+
+          done()
+        })
+    })
+  })
+
+  describe('Replies', function () {
+    before(function (done) {
+      const text = 'reply1'
+      const delete_password = 'delete reply1'
+
+      chai.request(server)
+        .post(REPLIESENDPOINT)
+        .send({
+          text,
+          delete_password,
+          thread_id: thread1Id
+        })
+        .end(function (err, res) {
+        })
+
+      chai.request(server)
+        .post(REPLIESENDPOINT)
+        .send({
+          text: 'reply2',
+          delete_password: 'delete reply2',
+          thread_id: thread1Id
+        })
+        .end(function (err, res) {
+          done()
+        })
+    })
+
+    it('#6) creating a new reply', function (done) {
+      chai.request(server)
+        .get(REPLIESENDPOINT + '?thread_id=' + thread1Id)
+        .end(function (err, res) {
+          const text = 'reply1'
+          const date = new Date()
+
+          assert.equal(res.status, 200)
+          assert.equal(res.body.replies[0].text, text);
+          assert.equal(new Date(res.body.replies[0].created_on).toDateString(), date.toDateString())
+          assert.equal(res.body._id, thread1Id);
+          assert.equal(res.body.bumped_on, res.body.replies[1].created_on);
+
+          reply1Id = res.body.replies[0]._id
+          reply2Id = res.body.replies[1]._id
+
+          done()
+        })
+    })
+
+    it('#7) viewing a single thread with all replies', function (done) {
+      chai.request(server)
+        .get(REPLIESENDPOINT + '?thread_id=' + thread1Id)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.containsAllKeys(res.body, ["_id", "text", "created_on", "bumped_on", "replies"]);
+          assert.isArray(res.body.replies);
+          assert.notExists(res.body.delete_password);
+          assert.notExists(res.body.reported);
+          for (let i = 0; i < res.body.replies.length; i++) {
+            assert.notExists(res.body.replies[i].delete_password);
+            assert.notExists(res.body.replies[i].reported);
+          }
+
+          done()
+        })
+    })
+
+    it('#8) deleting a reply with the incorrect password', function(done) {
+      chai.request(server)
+        .delete(REPLIESENDPOINT + '?thread_id=' + thread1Id)
+        .send({
+          thread_id: thread1Id,
+          reply_id: reply1Id,
+          delete_password: 'incorrect password'
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.not_deleted, 'incorrect password')
+          
+          done()
+        })
+    })
+
+    it('#9) deleting a reply with the correct password', function(done) {
+      chai.request(server)
+        .delete(REPLIESENDPOINT + '?thread_id=' + thread1Id)
+        .send({
+          thread_id: thread1Id,
+          reply_id: reply1Id,
+          delete_password: 'delete reply1'
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.deleted, 'success')
+          
+          done()
+        })
+    })
+    
+    it('#10) reporting a reply', function (done) {
+      chai.request(server)
+        .put(REPLIESENDPOINT)
+        .send({
+          thread_id: thread1Id,
+          reply_id: reply2Id      
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200)
+          assert.equal(res.text, 'reported')
+
+          done()
+        })
+    })
+  })
 });
